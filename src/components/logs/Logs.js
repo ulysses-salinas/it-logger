653afbcab6c6279fd27b6de8fd4a04016c438484
Ulysses Salinas <ulysses.salinas88@gmail.com>
2020-10-24 14:54:56 -0500

    ClassName correction in Logs component

--- src/components/logs/Logs.js
+++ src/components/logs/Logs.js
@@ -5,32 +5,36 @@ const Logs = () => {
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-      getLogs();
-      //esling-disable-next-line
-  }, [])
+    getLogs();
+    //esling-disable-next-line
+  }, []);
 
   const getLogs = async () => {
-      setLoading(true);
-      const res = await fetch('/logs')
-      const data = await res.json();
+    setLoading(true);
+    const res = await fetch('/logs');
+    const data = await res.json();
 
-      setLogs(data);
-      setLoading(false)
-  }
+    setLogs(data);
+    setLoading(false);
+  };
 
-  if(loading){
-      return <h4>Loading...</h4>
+  if (loading) {
+    return <h4>Loading...</h4>;
   }
-  return <div>
-      <ul className="collection-with-header">
-          <li className="collection-header">
-              <h4 className="center">System Logs</h4>
-          </li>
-          {!loading && logs.length === 0 ? (<p className="center">No logs to show...</p>) : (
-              logs.map(log => <li>{log.message}</li>)
-          )}
+  return (
+    <div>
+      <ul className='collection with-header'>
+        <li className='collection-header'>
+          <h4 className='center'>System Logs</h4>
+        </li>
+        {!loading && logs.length === 0 ? (
+          <p className='center'>No logs to show...</p>
+        ) : (
+          logs.map(log => <li>{log.message}</li>)
+        )}
       </ul>
-  </div>;
+    </div>
+  );
 };
 
 export default Logs;
