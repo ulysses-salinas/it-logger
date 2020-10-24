a95ca79a8e274783d35525ebb37e660c3b85c519
Ulysses Salinas <ulysses.salinas88@gmail.com>
2020-10-24 15:18:17 -0500

    Ternary Preloader component

--- src/components/logs/Logs.js
+++ src/components/logs/Logs.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from 'react';
+import LogItem from './LogItem';
+import Preloader from '../layout/Preloader';
 
 const Logs = () => {
   const [logs, setLogs] = useState([]);
@@ -19,7 +21,7 @@ const Logs = () => {
   };
 
   if (loading) {
-    return <h4>Loading...</h4>;
+    return <Preloader/>;
   }
   return (
     <div>
@@ -30,7 +32,7 @@ const Logs = () => {
         {!loading && logs.length === 0 ? (
           <p className='center'>No logs to show...</p>
         ) : (
-          logs.map(log => <li>{log.message}</li>)
+          logs.map(log => <LogItem log={log} key={log.id} />)
         )}
       </ul>
     </div>
