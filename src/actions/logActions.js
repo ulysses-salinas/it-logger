b50c9132495db4435c1e5c83640c8e47db2ed557
Ulysses Salinas <ulysses.salinas88@gmail.com>
2020-11-07 20:29:58 -0600

    Actions for logs, loading, error:

---
+++ src/actions/logActions.js
@@ -0,0 +1,41 @@
+import { GET_LOGS, SET_LOADING, LOGS_ERROR } from './types';
+
+// export const getLogs = () => {
+//     return async (dispatch) => {
+//         setLoading();
+
+//         const res = await fetch('/logs');
+//         const data = await res.json();
+
+//         dispatch({
+//             type: GET_LOGS,
+//             payload: dat
+//         })
+//     }
+// }
+
+export const getLogs = () => async dispatch => {
+  try {
+    setLoading();
+
+    const res = await fetch('/logs');
+    const data = await res.json();
+
+    dispatch({
+      type: GET_LOGS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: err.response.data,
+    });
+  }
+};
+
+// Set loading to true
+export const setLoading = () => {
+  return {
+    type: SET_LOADING,
+  };
+};
